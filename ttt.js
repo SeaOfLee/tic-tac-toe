5f9f96ce280f513d2df74bae00f47b14d9911a31
SeaOfLee <l.richardson1@gmail.com>
2015-01-21 08:56:21 -0800

    Add onclick function to JS

--- ttt.js
+++ ttt.js
@@ -1,2 +1,46 @@
 // JS file for TTT
-console.log("JS File works");
+var topLeft = document.getElementById("topLeft");
+var topMid = document.getElementById("topMid");
+var topRight = document.getElementById("topRight");
+var midLeft = document.getElementById("midLeft");
+var midMid = document.getElementById("midMid");
+var midRight = document.getElementById("midRight");
+var botLeft = document.getElementById("botLeft");
+var botMid = document.getElementById("botMid");
+var botRight = document.getElementById("botRight");
+
+topLeft.onclick = function() {
+  console.log("clicked top left");
+};
+
+topMid.onclick = function() {
+  console.log("clicked top middle");
+};
+
+topRight.onclick = function() {
+  console.log("clicked top right");
+};
+
+midLeft.onclick = function() {
+  console.log("clicked middle left");
+};
+
+midMid.onclick = function() {
+  console.log("clicked middle square");
+};
+
+midRight.onclick = function() {
+  console.log("clicked middle right");
+};
+
+botLeft.onclick = function() {
+  console.log("clicked bottom left");
+};
+
+botMid.onclick = function() {
+  console.log("clicked bottom middle");
+};
+
+botRight.onclick = function() {
+  console.log("clicked bottom right");
+};
